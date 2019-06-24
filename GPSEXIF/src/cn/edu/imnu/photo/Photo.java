8dd7f59646936b2fe66ced511eca3899c57f766a
xxxinymph20171105406 <43673484+xxxinymph20171105406@users.noreply.github.com>
2019-06-24 14:37:52 +0800

    换成了Web工程，添加了百度地图，上次的问题解决了。
    添加了百度地图，但是暂且地图和图片连不到一起。
    上次图片高度，图片宽度会连续出现。加了Exif就好了。

---
+++ GPSEXIF/src/cn/edu/imnu/photo/Photo.java
@@ -0,0 +1,62 @@
+package cn.edu.imnu.photo;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+
+import com.drew.imaging.ImageMetadataReader;
+import com.drew.imaging.ImageProcessingException;
+import com.drew.metadata.Directory;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.Tag;
+
+
+public class Photo {
+	public static void main(String[] args) throws Exception, Exception{
+		File file = new File("C:\\Users\\hp\\Desktop\\java\\meal.jpg");
+		System.out.println("File Name: " + file.getName());
+		printImageTags(file);
+	}
+	private static void printImageTags(File file) throws ImageProcessingException ,Exception{
+		Metadata metadata = ImageMetadataReader.readMetadata(file);
+		for(Directory directory : metadata.getDirectories()){
+			for(Tag tag : directory.getTags()) {
+				String tagName = tag.getTagName();
+				String info = tag.getDescription();
+				if(tagName.equals("Exif Image Height")) {
+					System.out.println("图片高度 ： " + info);
+				    //JOptionPane.showMessageDialog(null, "图片高度:" + info);
+				} 
+				if(tagName.equals("Exif Image Width")) {
+					System.out.println("图片宽度： " + info);
+					//JOptionPane.showMessageDialog(null, "图片宽度:" + info);
+				}
+				if(tagName.equals("Size")) {
+					System.out.println("图片大小： " + info);
+				}
+				if(tagName.equals("Exposure Time")) {
+					System.out.println("曝光时间： " + info);
+				}
+				if(tagName.equals("Date/Time Original")) {
+					System.out.println("拍摄时间： " + info);
+					JOptionPane.showMessageDialog(null, "拍摄时间:" + info);
+				}
+				if(tagName.equals("GPS Latitude")) {
+					System.out.println("纬度：" + info);
+					//JOptionPane.showMessageDialog(null, "纬度:" + info);
+				}
+				if(tagName.equals("GPS Longitude")) {
+					System.err.println("经度： " + info);
+					//JOptionPane.showMessageDialog(null, "经度:" + info);
+				}
+				if(tagName.equals("Model")) {
+					System.out.println("型号： " + info);
+				}
+				
+			}
+		}
+		
+	}
+}  
+	
