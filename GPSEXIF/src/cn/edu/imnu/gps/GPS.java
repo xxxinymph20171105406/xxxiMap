0b029abd58e59c566e372f76e397a7dbed919593
xxxinymph20171105406 <43673484+xxxinymph20171105406@users.noreply.github.com>
2019-07-03 21:49:43 +0800

    根据经纬度在地图上标识出来，有error

--- GPSEXIF/src/cn/edu/imnu/gps/GPS.java
+++ GPSEXIF/src/cn/edu/imnu/gps/GPS.java
@@ -1,21 +1,65 @@
 package cn.edu.imnu.gps;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
+import java.text.DecimalFormat;
 
 import net.sf.json.JSON;
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 
 public class GPS {
+	static String AK = "VUR55hBFz2tmn6IFDkHbaZh0V0iLgnQZ"; // 百度地图密钥
 
-	   
-	
+    public static void main(String[] args) {
+        String dom = "北京王府井";
+        String coordinate = getCoordinate(dom);
+        System.out.println("'" + dom + "'的经纬度为：" + coordinate);
+        
+    }
+
+   
+    public static String getCoordinate(String address) {
+        if (address != null && !"".equals(address)) {
+            address = address.replaceAll("\\s*", "").replace("#", "栋");
+            String url = "http://api.map.baidu.com/geocoder/v2/?address=" + address + "&output=json&ak=" + AK;
+            String json = loadJSON(url);
+            if (json != null && !"".equals(json)) {
+                JSONObject obj = JSONObject.fromObject(json);
+                if ("0".equals(obj.getString("status"))) {
+                    double lng = obj.getJSONObject("result").getJSONObject("location").getDouble("lng"); // 经度
+                    double lat = obj.getJSONObject("result").getJSONObject("location").getDouble("lat"); // 纬度
+                    DecimalFormat df = new DecimalFormat("#.######");
+                    return df.format(lng) + "," + df.format(lat);
+                }
+            }
+        }
+        return null;
+    }
+
+    public static String loadJSON(String url) {
+        StringBuilder json = new StringBuilder();
+        try {
+            URL oracle = new URL(url);
+            URLConnection yc = oracle.openConnection();
+            BufferedReader in = new BufferedReader(new InputStreamReader(yc.getInputStream(), "UTF-8"));
+            String inputLine = null;
+            while ((inputLine = in.readLine()) != null) {
+                json.append(inputLine);
+            }
+            in.close();
+        } catch (MalformedURLException e) {} catch (IOException e) {}
+        return json.toString();
+}
+
+    
 
 
-	
-
+    
 }
